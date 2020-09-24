8d96ed4f2a9f3459957bb1d9fd9e9458135db3d8
InnovativeInventor <theinnovativeinventor@gmail.com>
2020-09-24 14:34:59 -0400

    Added MCMC trait

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -1,12 +1,14 @@
 use crate::ballot::Ballot;
 use crate::ballot::Student;
 
-trait Optimizer {
+pub trait Optimizer {
     fn new(ballots: &'static Ballot) -> Self; //
 
     fn ballots(&self) -> &'static Ballot;
 
     fn optimize(&self) -> Vec<Vec<Student>>;
+
+    fn objective(&self) -> f64; // the objective function
 }
 
 impl Identity{
@@ -22,7 +24,33 @@ impl Identity for Optimizer{
 
     fn optimize(&self) -> Vec<Vec<Student>> {
         let iter = self.ballots.iter();
-        let schedule = vec![vec![Student], self.ballots.len(), self.ballots.len()]
-        return schedule
+        let schedule = vec![vec![Student], self.ballots.len(), self.ballots.len()];
+        return schedule;
+    }
+
+    fn objective(&self) -> f64 {
+        return 0;
+    }
+}
+
+pub trait MCMCOptimizer: Optimizer {
+    fn acceptance(&self, (i32, i32), (i32, i32)) -> f64;
+}
+
+impl MCMC{
+    
+}
+
+impl MCMC for MCMCOptimizer{
+    fn new(ballots: &'static Ballot) -> Identity{
+        Identity {
+            ballots: Ballot
+        }
+    }
+
+    fn optimize(&self) -> Vec<Vec<Student>> {
+        let iter = self.ballots.iter();
+        let schedule = vec![vec![Student], self.ballots.len(), self.ballots.len()];
+        return schedule;
     }
 }
