38b2328a98b49f602a6e6c75ad16e58fbc824c4e
Max Fan <theinnovativeinventor@gmail.com>
2020-10-03 19:34:11 -0400

    Made dice rolling non-deterministic so we don't get the same result if the step function is run multiple times in a row due to seed reuse

--- src/optimizers/mcmc.rs
+++ src/optimizers/mcmc.rs
@@ -3,6 +3,7 @@ pub mod mcmc_naive;
 use optimizers::Optimizer;
 use optimizers::rand::SeedableRng;
 use optimizers::rand::Rng;
+use super::rand::{thread_rng};
 use ballot::Student;
 
 #[derive(Debug, Clone)]
@@ -27,14 +28,18 @@ pub(self) trait MCMCOptimizer: Optimizer {
 
     fn propose(&self, schedule: &Vec<Vec<Student>>) -> Proposal;
 
-    fn step(&self, mut schedule: Vec<Vec<Student>>) -> Vec<Vec<Student>> { // steps through one iteration of the MCMC chain
-        let mut rng = rand::rngs::StdRng::seed_from_u64(0);
+    fn dice(&self) -> f64 {
+        // let mut rng = rand::rngs::StdRng::seed_from_u64(0);
+        let mut rng = thread_rng();
         let dice: f64 = rng.gen_range(0 as f64, 1 as f64);
+        return dice;
+    }
 
+    fn step(&self, mut schedule: Vec<Vec<Student>>) -> Vec<Vec<Student>> { // steps through one iteration of the MCMC chain
         let proposed_change: Proposal = self.propose(&schedule);
         let acceptance_prob: f64 = self.acceptance(&schedule,proposed_change.clone());
 
-        if dice < acceptance_prob { // proposal accepted
+        if self.dice() < acceptance_prob { // proposal accepted
             let mut student = schedule[proposed_change.student_location.0].remove(proposed_change.student_location.1);
             schedule[proposed_change.proposed_house].push(student);
         }
