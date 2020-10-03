741f7be802ab2396fa880d65089e680602c32dc6
Max Fan <theinnovativeinventor@gmail.com>
2020-10-03 19:21:17 -0400

    Added MCMC step function for all MCMC optimizers

--- src/optimizers/mcmc.rs
+++ src/optimizers/mcmc.rs
@@ -1,8 +1,11 @@
 pub mod mcmc_naive;
 
 use optimizers::Optimizer;
+use optimizers::rand::SeedableRng;
+use optimizers::rand::Rng;
 use ballot::Student;
 
+#[derive(Debug, Clone)]
 pub(self) struct Proposal {
     pub(self) student_location: (usize, usize),
     pub(self) proposed_house: usize
@@ -19,11 +22,26 @@ impl Proposal {
 pub(self) trait MCMCOptimizer: Optimizer {
     // An acceptance function takes in a particular location of the student (house, student) and the new house and returns a probability between 0-1 of acceptance.
     // 1 means a 100% probability of accepting
-    fn acceptance(&self, schedule: Vec<Vec<Student>>, proposal: Proposal) -> f64;
+    fn acceptance(&self, schedule: &Vec<Vec<Student>>, proposal: Proposal) -> f64;
     // A proposal function samples from all the house-student pairs and returns a students random change ((house, student), new_house).
-    fn propose(&self, schedule: Vec<Vec<Student>>) -> Proposal;
-}
 
+    fn propose(&self, schedule: &Vec<Vec<Student>>) -> Proposal;
+
+    fn step(&self, mut schedule: Vec<Vec<Student>>) -> Vec<Vec<Student>> { // steps through one iteration of the MCMC chain
+        let mut rng = rand::rngs::StdRng::seed_from_u64(0);
+        let dice: f64 = rng.gen_range(0 as f64, 1 as f64);
+
+        let proposed_change: Proposal = self.propose(&schedule);
+        let acceptance_prob: f64 = self.acceptance(&schedule,proposed_change.clone());
+
+        if dice < acceptance_prob { // proposal accepted
+            let mut student = schedule[proposed_change.student_location.0].remove(proposed_change.student_location.1);
+            schedule[proposed_change.proposed_house].push(student);
+        }
+
+        return schedule
+    }
+}
 
 fn normalize(student: Student) -> Student {
     unimplemented!();
