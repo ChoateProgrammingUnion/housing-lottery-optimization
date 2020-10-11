69b3874036268189f47e99d2b381d7402951376f
Aiyu Kamate <55467249+Nippon2019@users.noreply.github.com>
2020-10-11 14:38:30 +0900

    Update generate_ballots.py

--- generate_ballots.py
+++ generate_ballots.py
@@ -8,13 +8,14 @@ houses = ["Tenny", "CK", "East Cottage", "KEC", "Pratt"]  # add houses
 def randhouse(house):
     x = zipf(
         a=1.01 + random.random() / 2, size=1000
-    )  # use zipf distribution with some randomness in the variability of the distribution
+    )  # use zipf distribution with some randomness in the variability of the distribution, a = 1-2 because larger number of distribution makes one number too popular
     return (
         10
         - (int(random.choice(x[x < 6])) * 2 - random.randint(0, 1))
         - int(house / (len(houses) - 1) * 8)
         - random.randint(-1, 1)
     ) % 10 + 1.0  # return a number between 1-10, change popularity between houses so that some more are popular than others
+   # chooses a random number between 1-5 from the zipf list, *2 to make it 2,4,6,8,10, then minus 1 randomly to allow all integers from 1-10. Then, shift the distribution to make different popularities.
 
 
 def create():
