78a36c168839af930b93f592afd34067f33ea6c1
Ian <ianhaile@me.com>
2020-10-17 14:58:59 -0400

    Creates minimax algorithm (#22)
    * Fixes error in deans algorithm, and updates scale an normalize functions
    
    * Creates minimax algorithm

--- src/main.rs
+++ src/main.rs
@@ -30,13 +30,16 @@ fn main() {
             .output()
             .expect("failed to execute process");
             
-    let ballot = input::load_input(ballot::normalize);
-    //let ballot = input::load_input(ballot::normalize);
+
+    let ballot = input::load_input(ballot::identity);
+    // let ballot = input::load_input(ballot::normalize);
     crate::log_info!("successfully processed", "input");
 
-    let mut identity = optimizers::mcmc::mcmc_naive::MCMCNaive::new(&ballot);
     // let mut identity = optimizers::multi_dist::MultiDist::new(&ballot, 0, 10.0);
-    // let mut identity = optimizers::deans_algorithm::DeansAlgorithm::new(&ballot);
+    let mut identity = optimizers::mcmc::minimax::Minimax::new(&ballot);
+    //let mut identity = optimizers::mcmc::mcmc_naive::MCMCNaive::new(&ballot);
+    //let mut identity = optimizers::deans_algorithm::DeansAlgorithm::new(&ballot);
+
 
     // println!("How many rounds?");
     // let mut rounds_input = String::new();
@@ -44,6 +47,7 @@ fn main() {
     //     .read_line(&mut rounds_input)
     //     .expect("Not a valid input!");
     // let rounds = rounds_input.trim().parse::<usize>().expect("Not a usize");
+    
     for x in 0..4 {
         let num: usize = 10;
         let mut rounds: usize = 1*num.pow(x as u32);
@@ -58,6 +62,7 @@ fn main() {
         output::write_output(&result, &ballot);
         data_output::write_output(&result, &ballot, &optimized_time, &(x+1).to_string());
         crate::log_info!("finished", "output");
+        
     }
     Command::new("python3")
             .arg("graph.py")
---
+++ src/optimizers/mcmc/minimax.rs
@@ -0,0 +1,106 @@
+use ballot::{Ballot, Student};
+use optimizers::mcmc::{MCMCOptimizer, Proposal};
+use optimizers::{Optimizer, generate_random_allocation};
+
+pub struct Minimax{
+    ballots: Ballot
+}
+
+impl Minimax {
+    pub fn new(ballots: &Ballot) -> Self {
+        Self {
+            ballots: ballots.clone()
+        }
+    }
+    fn size(&self , schedule: Vec<Vec<Student>>) -> (Vec<Vec<Student>>, usize) {
+        let mut counter = 0;
+        for house in &schedule {
+            counter += house.len();
+        }
+        return (schedule, counter);
+    }
+}
+
+impl MCMCOptimizer for Minimax{
+    fn acceptance(&self, schedule: &Vec<Vec<Student>>, proposal: Proposal) -> f64 {
+        // if current house is worse, chance of staying is current rank^(-2)
+        // if current house is better, chance of moving is new rank^(-2)
+        let student: &Student = &schedule[proposal.student_location.0][proposal.student_location.1];
+
+        let mut current_house_rank = 0;
+        let current_house_score = &student.ballot[proposal.student_location.0];
+
+        let mut new_house_rank = 0;
+        let new_house_score = &student.ballot[proposal.proposed_house];
+
+        // finds how many houses are higher scored than the house in question so the rank can be determined
+        for house in &student.ballot{
+            if house > current_house_score {
+                current_house_rank+=1;
+            }
+            if house > new_house_score {
+                new_house_rank+=1;
+            }
+        }
+
+        if new_house_rank > current_house_rank {
+            let probability: f64 = 1.0 - (current_house_rank^(-2)) as f64;
+            return probability;
+        } else {
+            let probability: f64 = (new_house_rank^(-2)) as f64;
+            return probability;
+        }
+    }
+
+    fn propose(&self, schedule: &Vec<Vec<Student>>) -> Proposal {
+        // Uniform, random sampling
+        let size = self.ballots.students.len();
+
+        let mut student_location = self.gen_range(0, size);
+        let mut new_house = self.gen_range(0, schedule.len() -1);
+
+
+        let mut counter: usize = 0;
+        let mut current_house: usize = 0;
+        let mut current_index: usize = 0;
+
+        'house: for house in schedule {
+            for student in house {
+                if counter == student_location {
+                    break 'house;
+                }
+                counter += 1;
+                current_index += 1;
+            }
+            current_index = 0;
+            current_house += 1;
+        }
+
+        if new_house >= current_house { // ensure we don't get the same house
+            new_house += 1;
+        }
+
+        let proposed_change = Proposal{
+            student_location: (current_house, current_index),
+            proposed_house: new_house
+        };
+
+        return proposed_change
+    }
+}
+
+impl Optimizer for Minimax {
+    fn optimize(&mut self, rounds: usize) -> Vec<Vec<Student>> {
+        let mut schedule: Vec<Vec<Student>> = generate_random_allocation(&self.ballots, 0 as u64);
+        for round in 0..rounds{
+            schedule = self.step(schedule);
+        }
+        return schedule;
+    }
+
+    fn objective(&self) -> f64 {
+        return 0.0;
+    }
+}
+
+
